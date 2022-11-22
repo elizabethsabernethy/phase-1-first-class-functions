359eb099657e72c3af0e24118332e73ba1f201f0
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-22 15:38:33 -0500

    commit tree 8f5b1415142893eac4984d777b8deef041aead64

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+return callback();
+}
+
+function returnsANamedFunction(){
+return function named(){
+
+}
+}
+
+function returnsAnAnonymousFunction(){
+return function(){
+    
+}
+}
